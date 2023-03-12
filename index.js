e19d5ab19fb7fe30563aed3ab7456e2b36cfed27
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-12 17:25:55 +0200

    Adding Cake reducer and Icecream separatly

--- index.js
+++ index.js
@@ -22,17 +22,34 @@ function buyIceCream() {
 
 
 
-const initialState = {
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+// Multiple reducer
+
+const initialCakeState = {
     numOfCakes: 10,
+}
+
+const initialIceCreamState = {
     numOfIceCreams: 20
 }
 
-const reducer =(state = initialState,action) => {
+const cakeReducer =(state = initialCakeState,action) => {
     switch(action.type) {
         case BUY_CAKE: return {
             ... state,
             numOfCakes: state.numOfCakes - 1
         }
+        default: return state
+    }
+}
+
+const iceCreamReducer =(state = initialIceCreamState,action) => {
+    switch(action.type) {
+      
         case BUY_ICECREAM: return {
             ... state,
             numOfIceCreams: state.numOfIceCreams - 1
