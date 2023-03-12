1a2a551cb3f87f5a167a660aff51cb867cde5f6e
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-12 14:02:11 +0200

    Unsubsciber method

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const reducer =(state = initialState,action) => {
 
 const store = createStore(reducer)
 console.log('Initial state',store.getState())
-store.subscribe(() => console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+unsubscribe()
